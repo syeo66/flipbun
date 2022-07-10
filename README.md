4cacd24cbcaf76fdc59191921dfeca1b8b2d7867
Red Ochsenbein <red@redochsenbein.ch>
2022-07-10 10:02:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Some FLIP animation experiments using bun
+
+## Start local development server
+
+Use `bun dev` in the project root to start the development server. Then go to `http://localhost:3000`.
+
+Read about [bun](https://bun.sh).
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+const block1 = document.querySelector<HTMLDivElement>('#block1')
+
+const positions: Element[] = [
+  document.querySelector('.middle'),
+  document.querySelector('.end'),
+  document.querySelector('.start'),
+]
+let index = 0
+
+block1.addEventListener('click', () => {
+  const start = block1.getBoundingClientRect()
+
+  positions[index].appendChild(block1)
+
+  const end = block1.getBoundingClientRect()
+
+  const dx = end.left - start.left
+  const dy = end.top - start.top
+
+  block1.animate(
+    [
+      {
+        transform: `translate(${-dx}px, ${-dy}px)`,
+      },
+      {
+        transform: 'translate(0px, 0px)',
+      },
+    ],
+    { duration: 300, fill: 'both' }
+  )
+
+  index = (index + 1) % positions.length
+})
